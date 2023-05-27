fa0a05bb168cdeeab7bf63d8c4bd0c86ca096f4e
Vinay kumar <114425298+ursvinaykumar@users.noreply.github.com>
2023-05-27 22:23:56 +0530

    Add files via upload

--- chimtu.py
+++ chimtu.py
@@ -1,69 +1,64 @@
-import speech_recognition as aa
+import speech_recognition as sr
 import pyttsx3
 import pywhatkit
 import datetime
 import wikipedia
 
-listener=aa.Recognizer()
-
-machine=pyttsx3.init()
+listener = sr.Recognizer()
+machine = pyttsx3.init()
 
 def talk(text):
     machine.say(text)
     machine.runAndWait()
 
 def input_instruction():
-    global instrction
-
+    global instruction
 
     try:
-        with aa.Microphone() as origin:
-            print("listening...")
-            speech = listener.listen(origin)
-            instruction=listener.recognize_google(speech)
-            instruction=instruction.lower()
+        with sr.Microphone() as source:
+            print("Listening...")
+            audio = listener.listen(source)
+            instruction = listener.recognize_google(audio)
+            instruction = instruction.lower()
             if "chimtu" in instruction:
-                instruction=instruction.replace('chimtu','')
-                print(instruction)
+                instruction = instruction.replace('chimtu', '')
             print(instruction)
 
-
-    except:
+    except sr.UnknownValueError:
         pass
+
     return instruction
 
 def play_chimtu():
-    instruction=input_instruction()
+    instruction = input_instruction()
     print(instruction)
 
     if "play" in instruction:
-        song=instruction.replace('play',"")
-        talk("playing" + song)
+        song = instruction.replace('play', '')
+        talk("Playing " + song)
         pywhatkit.playonyt(song)
 
     elif 'time' in instruction:
-        time=datetime.datetime.now().strftime('%I:%M%p')
-        talk('Current time'+ time)
+        current_time = datetime.datetime.now().strftime('%I:%M %p')
+        talk('The current time is ' + current_time)
 
     elif 'date' in instruction:
-        date= datetime.datetime.now().strftime('%d /%m /%Y')
-        talk("Today's date "+ date)
+        current_date = datetime.datetime.now().strftime('%d/%m/%Y')
+        talk("Today's date is " + current_date)
 
     elif 'how are you' in instruction:
-        talk('I am fine,how about you')
+        talk('I am fine, how about you?')
 
-    elif 'What is your name' in instruction:
-        talk('I am chimtu, what can I do for you?')
+    elif 'what is your name' in instruction:
+        talk('I am Chimtu, what can I do for you?')
 
-    elif 'Who is' in instruction:
-        human = instruction.replace('who is', " ")
-        info = wikipedia.summary(human, 1)
+    elif 'who is' in instruction:
+        person = instruction.replace('who is', '').strip()
+        info = wikipedia.summary(person, 1)
         print(info)
         talk(info)
 
-
     else:
         talk('Please repeat')
-
 
 play_chimtu()
